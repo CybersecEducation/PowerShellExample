7c234a4244d6d83fe6e2a20cb78da929be88cdce
CyberSec Education, LLC <32116203+CybersecEducation@users.noreply.github.com>
2017-09-19 21:45:04 -0400

    Update example.ps1

--- example.ps1
+++ example.ps1
@@ -1,6 +1,6 @@
 
 
-write-host "CyberSec Education PowerShell Example"
-write-host "This script was loaded from Github and run on your local machine."
+write-host "CyberSec Education PowerShell Example" -foregroundcolor green
+write-host "This script was loaded from Github and run on your local machine." -foregroundcolor green
 $psversiontable 
-hostname
+
